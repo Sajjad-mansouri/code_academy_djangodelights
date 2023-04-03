77bf1b420a09808fb02c1621d045ff1cccfbfeec
Sajjad Mansouri <s.mansouri4w@gmail.com>
2023-04-03 10:53:02 +0430

    create model and admin for project

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Ingredient, MenuItem,Purchase
 
-# Register your models here.
+class IngredientInline(admin.TabularInline):
+    model = MenuItem.ingredient.through
+    extra = 1
+
+class MenuItemAdmin(admin.ModelAdmin):
+    inlines = [IngredientInline]
+    exclude=('ingredient',)
+
+admin.site.register(MenuItem,MenuItemAdmin)
+admin.site.register(Ingredient)
+
+class PurchaseAdmin(admin.ModelAdmin):
+    list_display=['pk','orders','order_time']
+admin.site.register(Purchase,PurchaseAdmin)
--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
 # Create your models here.
+
+class Ingredient(models.Model):
+	#add name of ingredient
+	#add price per unit
+	#how much item available
+	UNIT_CHOICES=[
+	('l','litres'),
+	('ml','millilitres'),
+	('g','grams'),
+	('kg','kilograms'),
+	('oz','ounce'),
+	('co','count')]
+	title=models.CharField(max_length=50)
+	unit=models.CharField(max_length=2,choices=UNIT_CHOICES)
+	quantity=models.FloatField(help_text='(Kg)')
+	price=models.FloatField(help_text='$')
+	def __str__(self):
+		return self.title
+
+
+class MenuItem(models.Model):
+	title=models.CharField(max_length=50)
+	ingredient=models.ManyToManyField(Ingredient,through='RecipeRequirement')
+	price=models.FloatField(help_text='$')
+	
+	def __str__(self):
+		return self.title
+
+		
+
+class RecipeRequirement(models.Model):
+	ingredient=models.ForeignKey(Ingredient,on_delete=models.CASCADE)
+	menu=models.ForeignKey(MenuItem,on_delete=models.CASCADE)
+	quantity=models.FloatField()
+		
+
+class Purchase(models.Model):
+	#add customer purchase
+	#inventory should be modified
+	#record time that purches was made
+	order=models.ManyToManyField(MenuItem)
+	order_time=models.DateTimeField(auto_now_add=True)
+
+	def orders(self):
+		return ','.join([item.title for item in self.order.all()])
